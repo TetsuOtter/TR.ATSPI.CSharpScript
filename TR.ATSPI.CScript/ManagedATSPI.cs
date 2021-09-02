13518145a1f913667b227477df2d44f9f1c4bdff
ＴＲ <public@technotter.com>
2021-09-02 23:38:05 +0900

    不要なデバッグ用実装を削除

--- TR.ATSPI.CScript/ManagedATSPI.cs
+++ TR.ATSPI.CScript/ManagedATSPI.cs
@@ -55,18 +55,13 @@ namespace TR.ATSPI.CScript
 		/// <returns></returns>
 		public bool LoadScriptPathListsAndScripts()
 		{
-			Debug.WriteLine(nameof(LoadScriptPathListsAndScripts) + "() ====================");
-
 			//設定ファイルを読み込む => XML
 			// C:\abcDef\TR.ATSPI.CScript.xml
 			string rootSettingFilePath = Path.Combine(CurrentDllDirectoryPath, CurrentDllFileNameWithoutExtension + ".xml");
 
-			Debug.WriteLine(rootSettingFilePath);
-
 			//ルートとなる設定ファイルが存在する場合のみ, 読み込みを実行
 			if (File.Exists(rootSettingFilePath))
 			{
-				Debug.WriteLine("XML Found");
 				try
 				{
 					//スクリプトファイルリストを読み込む
@@ -108,12 +103,10 @@ namespace TR.ATSPI.CScript
 			}
 			else
 			{
-				Debug.WriteLine("XML Not found");
-
 				//XMLのタグのみが含まれたファイルを書き出す
 				using StreamWriter sw = File.CreateText(rootSettingFilePath);
 				ScriptPathListClass splc = new();
-				splc.ElapseScripts.Add("abc.txt");
+
 				Serializer.Serialize(sw, splc);
 
 				//スクリプトのロードには失敗しているため
